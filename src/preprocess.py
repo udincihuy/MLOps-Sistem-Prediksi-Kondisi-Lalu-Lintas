e2b0c2852cae60741c5ef998b47ff82f29b5cfa0
udincihuy <samueladeputra25@gmail.com>
2026-06-23 15:58:23 +0000

    fix preprocess indentation

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,25 +3,21 @@ import pandas as pd
 def preprocess():
     file = "data/raw/oil.csv"
 
-        data = pd.read_csv(file)
+    data = pd.read_csv(file)
 
-   
     data = data.dropna()
     data = data.drop_duplicates(subset=["Date"])
 
     data['Date'] = pd.to_datetime(data['Date'])
     data = data.sort_values(by="Date")
 
-  
     data['lag1'] = data['Close'].shift(1)
     data['lag2'] = data['Close'].shift(2)
     data['ma3'] = data['Close'].rolling(3).mean()
 
-
     data['target'] = data['Close'].shift(-1)
 
     data = data.dropna()
-
 
     data = data[['Close', 'lag1', 'lag2', 'ma3', 'target']]
 
